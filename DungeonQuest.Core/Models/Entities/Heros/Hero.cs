6f041e38fcc6707a2e1a130f09a6d1ac9c2f1368
ADB-DevWorks <adam.brawley@snhu.edu>
2026-05-07 20:54:35 -0500

    Added inventory lists to new hero class and created empty classes

---
+++ DungeonQuest.Core/Models/Entities/Heros/Hero.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DungeonQuest.Core.Models.Items.Armor;
+using DungeonQuest.Core.Models.Items.Weapons;
+using DungeonQuest.Core.Models.Items.Consumables;
+using DungeonQuest.Core.Models.Items.Artifacts;
+
+namespace DungeonQuest.Core.Models.Entities.Heros
+{
+    internal abstract class Hero : Entity
+    {
+        public int Gold { get; set; }
+        public List<Weapon> Weapons { get; set; }
+        public List<Armor> Armor { get; set; }
+        public List<Consumable> Consumables { get; set; }
+        public List<Artifact> Artifacts { get; set; }
+    }
+}
